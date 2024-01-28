ce78d5f5563f28185f3d6cff68f973c632547b1e
BABA <692877906@qq.com>
2024-01-28 23:20:42 +0800

    channel-sync

---
+++ channel/channel-sync.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func worker(done chan bool) {
+	fmt.Println("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 1)
+	go worker(done)
+	<-done
+	f("Hello")
+}
